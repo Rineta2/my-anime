f30e0f9dc538854f6b37ce126ccb9beaa3eb3036
Rineta2 <rzkir571@gmail.com>
2025-04-28 10:42:00 +0700

    [update] optimasi next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,39 @@
 /** @type {import('next').NextConfig} */
 import bundleAnalyzer from "@next/bundle-analyzer";
+import { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "samehadaku.mba",
-      "ik.imagekit.io",
-      "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com",
-      "cover.komiku.id",
-      "cdn.komiku.id",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "samehadaku.mba",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cover.komiku.id",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.komiku.id",
+      },
     ],
+    unoptimized: true,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ["image/webp", "image/avif"] as ("image/webp" | "image/avif")[],
   },
 };
 
